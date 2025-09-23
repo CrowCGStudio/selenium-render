2c0815d837522b79ee882632143fd51c16006d9d
bats11 <70653789+bats11@users.noreply.github.com>
2025-09-23 18:20:16 +0200

    Revert "prova no headless"
    This reverts commit d5e413edc052877587305550006eaa2b4f00da45.

--- app.py
+++ app.py
@@ -40,21 +40,11 @@ def build_driver():
 
     options = Options()
     options.binary_location = chrome_binary
-
-    # 🔴 per debug: togliamo l'headless
-    # options.add_argument("--headless=new")
-
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
-
-    # 👤 impostiamo user-agent da Chrome reale
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
-    )
 
     service = Service(chromedriver_path)
     driver = webdriver.Chrome(service=service, options=options)
@@ -65,7 +55,6 @@ def build_driver():
     )
 
     return driver
-
 
 def guess_mime(filename: str) -> str:
     mt, _ = mimetypes.guess_type(filename)
